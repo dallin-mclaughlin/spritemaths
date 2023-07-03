7c75bd1eb8fb539581b8b401d326c583e7004858
Dallin McLaughlin <nillad12@gmail.com>
2023-07-03 22:24:57 +1200

    Updated spritemath welcome blurb

--- resources/js/Components/Welcome.vue
+++ resources/js/Components/Welcome.vue
@@ -10,17 +10,16 @@ import ApplicationLogo from "@/Components/ApplicationLogo.vue";
             <ApplicationLogo class="block h-12 w-auto" />
 
             <h1 class="mt-8 text-2xl font-medium text-gray-900 dark:text-white">
-                Welcome to your Jetstream application!
+                Welcome to Spritemaths!
             </h1>
 
             <p class="mt-6 text-gray-500 dark:text-gray-400 leading-relaxed">
-                Laravel Jetstream provides a beautiful, robust starting point
-                for your next Laravel application. Laravel is designed to help
-                you build your application using a development environment that
-                is simple, powerful, and enjoyable. We believe you should love
-                expressing your creativity through programming, so we have spent
-                time carefully crafting the Laravel ecosystem to be a breath of
-                fresh air. We hope you love it.
+                Spritemaths is the number one site to test your math knowledge
+                and problem solving skills. Learn how to navigate the
+                complexities of logic and algebraic manipulation. Learn how to
+                prepare well for those end of year exams. Spritemaths is founded
+                on the idea that randomised and targeted questions will help
+                cement mathematical ideas in your brain.
             </p>
         </div>
 
