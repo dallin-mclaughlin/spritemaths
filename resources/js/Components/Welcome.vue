2bfe441dbf6b3fedfc3c63e5c34204389143c837
Dallin McLaughlin <nillad12@gmail.com>
2023-07-03 22:33:35 +1200

    Removed two sections from the welcome page

--- resources/js/Components/Welcome.vue
+++ resources/js/Components/Welcome.vue
@@ -37,59 +37,6 @@ import ApplicationLogo from "@/Components/ApplicationLogo.vue";
                     >
                         <path
                             stroke-linecap="round"
-                            stroke-linejoin="round"
-                            d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25"
-                        />
-                    </svg>
-                    <h2
-                        class="ml-3 text-xl font-semibold text-gray-900 dark:text-white"
-                    >
-                        <a href="https://laravel.com/docs">Documentation</a>
-                    </h2>
-                </div>
-
-                <p
-                    class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed"
-                >
-                    Laravel has wonderful documentation covering every aspect of
-                    the framework. Whether you're new to the framework or have
-                    previous experience, we recommend reading all of the
-                    documentation from beginning to end.
-                </p>
-
-                <p class="mt-4 text-sm">
-                    <a
-                        href="https://laravel.com/docs"
-                        class="inline-flex items-center font-semibold text-indigo-700 dark:text-indigo-300"
-                    >
-                        Explore the documentation
-
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 20 20"
-                            class="ml-1 w-5 h-5 fill-indigo-500 dark:fill-indigo-200"
-                        >
-                            <path
-                                fill-rule="evenodd"
-                                d="M5 10a.75.75 0 01.75-.75h6.638L10.23 7.29a.75.75 0 111.04-1.08l3.5 3.25a.75.75 0 010 1.08l-3.5 3.25a.75.75 0 11-1.04-1.08l2.158-1.96H5.75A.75.75 0 015 10z"
-                                clip-rule="evenodd"
-                            />
-                        </svg>
-                    </a>
-                </p>
-            </div>
-
-            <div>
-                <div class="flex items-center">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke-width="1.5"
-                        class="w-6 h-6 stroke-gray-400"
-                    >
-                        <path
-                            stroke-linecap="round"
                             d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z"
                         />
                     </svg>
@@ -160,39 +107,6 @@ import ApplicationLogo from "@/Components/ApplicationLogo.vue";
                     first CSS framework that doesn't get in your way. You'll be
                     amazed how easily you can build and maintain fresh, modern
                     designs with this wonderful framework at your fingertips.
-                </p>
-            </div>
-
-            <div>
-                <div class="flex items-center">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke-width="1.5"
-                        class="w-6 h-6 stroke-gray-400"
-                    >
-                        <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z"
-                        />
-                    </svg>
-                    <h2
-                        class="ml-3 text-xl font-semibold text-gray-900 dark:text-white"
-                    >
-                        Authentication
-                    </h2>
-                </div>
-
-                <p
-                    class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed"
-                >
-                    Authentication and registration views are included with
-                    Laravel Jetstream, as well as support for user email
-                    verification and resetting forgotten passwords. So, you're
-                    free to get started with what matters most: building your
-                    application.
                 </p>
             </div>
         </div>
