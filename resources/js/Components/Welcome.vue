9b1b1db850e18c08a14663432588e9bd9d8c646f
Dallin McLaughlin <nillad12@gmail.com>
2023-07-03 22:41:53 +1200

    Added Quiz Page and Mathematical Theory summaries

--- resources/js/Components/Welcome.vue
+++ resources/js/Components/Welcome.vue
@@ -1,14 +1,10 @@
-<script setup>
-import ApplicationLogo from "@/Components/ApplicationLogo.vue";
-</script>
+<script setup></script>
 
 <template>
     <div>
         <div
             class="p-6 lg:p-8 bg-white dark:bg-gray-800 dark:bg-gradient-to-bl dark:from-gray-700/50 dark:via-transparent border-b border-gray-200 dark:border-gray-700"
         >
-            <ApplicationLogo class="block h-12 w-auto" />
-
             <h1 class="mt-8 text-2xl font-medium text-gray-900 dark:text-white">
                 Welcome to Spritemaths!
             </h1>
@@ -43,38 +39,20 @@ import ApplicationLogo from "@/Components/ApplicationLogo.vue";
                     <h2
                         class="ml-3 text-xl font-semibold text-gray-900 dark:text-white"
                     >
-                        <a href="https://laracasts.com">Laracasts</a>
+                        <a href="https://laracasts.com">Quiz Page</a>
                     </h2>
                 </div>
 
                 <p
                     class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed"
                 >
-                    Laracasts offers thousands of video tutorials on Laravel,
-                    PHP, and JavaScript development. Check them out, see for
-                    yourself, and massively level up your development skills in
-                    the process.
-                </p>
-
-                <p class="mt-4 text-sm">
-                    <a
-                        href="https://laracasts.com"
-                        class="inline-flex items-center font-semibold text-indigo-700 dark:text-indigo-300"
-                    >
-                        Start watching Laracasts
-
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 20 20"
-                            class="ml-1 w-5 h-5 fill-indigo-500 dark:fill-indigo-200"
-                        >
-                            <path
-                                fill-rule="evenodd"
-                                d="M5 10a.75.75 0 01.75-.75h6.638L10.23 7.29a.75.75 0 111.04-1.08l3.5 3.25a.75.75 0 010 1.08l-3.5 3.25a.75.75 0 11-1.04-1.08l2.158-1.96H5.75A.75.75 0 015 10z"
-                                clip-rule="evenodd"
-                            />
-                        </svg>
-                    </a>
+                    This page is where all the questions are generated and the
+                    learning takes place. Behind the scene algorithms are at
+                    work creating skill specific questions for you. This is the
+                    power of spritemaths. Every question should be unique so
+                    that you are learning to spot patterns and think logically
+                    about how to approach a problem instead of memorising the
+                    answer to a question.
                 </p>
             </div>
 
@@ -96,17 +74,19 @@ import ApplicationLogo from "@/Components/ApplicationLogo.vue";
                     <h2
                         class="ml-3 text-xl font-semibold text-gray-900 dark:text-white"
                     >
-                        <a href="https://tailwindcss.com/">Tailwind</a>
+                        <a href="https://tailwindcss.com/"
+                            >Mathematical Theory</a
+                        >
                     </h2>
                 </div>
 
                 <p
                     class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed"
                 >
-                    Laravel Jetstream is built with Tailwind, an amazing utility
-                    first CSS framework that doesn't get in your way. You'll be
-                    amazed how easily you can build and maintain fresh, modern
-                    designs with this wonderful framework at your fingertips.
+                    Here you can delve into the theory of mathematics. The
+                    theory is really quite beautiful. It all starts off from the
+                    building blocks called axioms. Everything else builds upon
+                    these fundamentals.
                 </p>
             </div>
         </div>
