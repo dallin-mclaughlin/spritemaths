d40ec813aa957dafe5f8909592088bd61db8b155
Dallin McLaughlin <nillad12@gmail.com>
2023-07-13 14:25:47 +1200

    Another commit

--- resources/js/Components/Quiz.vue
+++ resources/js/Components/Quiz.vue
@@ -1,57 +1,62 @@
-<script setup>
+<script setup lang="ts">
+import { ref, onMounted, onUpdated } from "vue";
+import { MathfieldElement, renderMathInElement } from "mathlive";
 import { Link } from "@inertiajs/vue3";
+import "../../css/mathlive-fonts.css";
+import "../../css/mathlive.css";
+
+const mathFieldAnswer = ref<MathfieldElement>();
+const keyboard = window.mathVirtualKeyboard;
+console.log(window);
+//keyboard.layouts = ["numeric", "symbols"];
 </script>
 
 <template>
     <div>
         <div
-            class="p-6 lg:p-8 bg-white dark:bg-gray-800 dark:bg-gradient-to-bl dark:from-gray-700/50 dark:via-transparent border-b border-gray-200 dark:border-gray-700"
+            class="p-6 lg:p-8 text-center bg-white dark:bg-gray-800 dark:bg-gradient-to-bl dark:from-gray-700/50 dark:via-transparent border-b border-gray-200 dark:border-gray-700"
         >
             <h1 class="mt-8 text-2xl font-medium text-gray-900 dark:text-white">
-                Welcome to the Quiz Page!
+                The Question
             </h1>
-
-            <p class="mt-6 text-gray-500 dark:text-gray-400 leading-relaxed">
-                Here we populate the page with randomly generated questions.
-            </p>
         </div>
 
         <div
             class="bg-gray-200 dark:bg-gray-800 bg-opacity-25 grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8 p-6 lg:p-8"
         >
+            <!-- // include the answer part here -->
+            <math-field
+                :ref="mathFieldAnswer"
+                plonkSound="none"
+                className="border-2 w-96 shadow-xl"
+                mathVirtualKeyboardPolicy="auto"
+                value="2x+3"
+            ></math-field>
             <div>
-                <div class="flex items-center">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke-width="1.5"
-                        class="w-6 h-6 stroke-gray-400"
+                <div>
+                    <button
+                        className="text-gray-500 dark:text-gray-400 border rounded-md"
                     >
-                        <path
-                            stroke-linecap="round"
-                            d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z"
-                        />
-                    </svg>
-                    <h2
-                        class="ml-3 text-xl font-semibold text-gray-900 dark:text-white"
+                        Previous Question
+                    </button>
+                    <button
+                        className="text-gray-500 dark:text-gray-400 border rounded-md"
                     >
-                        <Link :href="route('quiz')">Quiz Page</Link>
-                    </h2>
+                        Next Question
+                    </button>
                 </div>
-
-                <p
-                    class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed"
-                >
-                    This page is where all the questions are beautifully
-                    generated and the learning takes place. Behind the scene
-                    algorithms are at work creating skill specific questions for
-                    you. This is the power of spritemaths. Every question should
-                    be unique so that you are learning to spot patterns and
-                    think logically about how to approach a problem instead of
-                    memorising the answer to a question.
-                </p>
-                <button className="border rounded-md">Start Quiz</button>
+                <div>
+                    <button
+                        className="text-gray-500 dark:text-gray-400 border rounded-md"
+                    >
+                        Save and Exit
+                    </button>
+                    <button
+                        className="text-gray-500 dark:text-gray-400 border rounded-md"
+                    >
+                        Save and Mark
+                    </button>
+                </div>
             </div>
         </div>
     </div>
