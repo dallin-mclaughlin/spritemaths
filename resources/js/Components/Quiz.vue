19e139ee1ff2c5a8a5617d7ab102b48f45696c8f
Dallin McLaughlin <nillad12@gmail.com>
2023-07-17 11:21:35 +1200

    Added mathlive interface

--- resources/js/Components/Quiz.vue
+++ resources/js/Components/Quiz.vue
@@ -1,14 +1,13 @@
 <script setup lang="ts">
 import { ref, onMounted, onUpdated } from "vue";
-import { MathfieldElement, renderMathInElement } from "mathlive";
+import { MathfieldElement } from "mathlive";
 import { Link } from "@inertiajs/vue3";
 import "../../css/mathlive-fonts.css";
 import "../../css/mathlive.css";
 
-const mathFieldAnswer = ref<MathfieldElement>();
+const mathFieldAnswer = ref<MathfieldElement>(new MathfieldElement());
 const keyboard = window.mathVirtualKeyboard;
-console.log(window);
-//keyboard.layouts = ["numeric", "symbols"];
+keyboard.layouts = ["numeric"];
 </script>
 
 <template>
@@ -16,47 +15,44 @@ console.log(window);
         <div
             class="p-6 lg:p-8 text-center bg-white dark:bg-gray-800 dark:bg-gradient-to-bl dark:from-gray-700/50 dark:via-transparent border-b border-gray-200 dark:border-gray-700"
         >
+            <div className="flex justify-between">
+                <button
+                    className="text-gray-500 dark:text-gray-400 border-2 rounded-md px-4"
+                >
+                    Save and Exit
+                </button>
+                <button
+                    className="text-gray-500 dark:text-gray-400 border-2 rounded-md px-4"
+                >
+                    Save and Mark
+                </button>
+            </div>
             <h1 class="mt-8 text-2xl font-medium text-gray-900 dark:text-white">
                 The Question
             </h1>
         </div>
 
         <div
-            class="bg-gray-200 dark:bg-gray-800 bg-opacity-25 grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8 p-6 lg:p-8"
+            class="bg-gray-200 dark:bg-gray-800 bg-opacity-25 grid grid-cols-1 gap-6 lg:gap-8 p-6 lg:p-8 text-center"
         >
             <!-- // include the answer part here -->
             <math-field
-                :ref="mathFieldAnswer"
+                ref="mathFieldAnswer"
+                className="text-3xl justify-center"
                 plonkSound="none"
-                className="border-2 w-96 shadow-xl"
-                mathVirtualKeyboardPolicy="auto"
-                value="2x+3"
+                value="2+3"
             ></math-field>
-            <div>
-                <div>
-                    <button
-                        className="text-gray-500 dark:text-gray-400 border rounded-md"
-                    >
-                        Previous Question
-                    </button>
-                    <button
-                        className="text-gray-500 dark:text-gray-400 border rounded-md"
-                    >
-                        Next Question
-                    </button>
-                </div>
-                <div>
-                    <button
-                        className="text-gray-500 dark:text-gray-400 border rounded-md"
-                    >
-                        Save and Exit
-                    </button>
-                    <button
-                        className="text-gray-500 dark:text-gray-400 border rounded-md"
-                    >
-                        Save and Mark
-                    </button>
-                </div>
+            <div className="flex justify-between">
+                <button
+                    className="text-gray-500 dark:text-gray-400 border-2 rounded-md px-4"
+                >
+                    Previous Question
+                </button>
+                <button
+                    className="text-gray-500 dark:text-gray-400 border-2 rounded-md px-4"
+                >
+                    Next Question
+                </button>
             </div>
         </div>
     </div>
